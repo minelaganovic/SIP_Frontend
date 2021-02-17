9ed39f86d73712690d8a924117ba75de3bedbfb8
Minela Ganovic <59210673+minelaganovic@users.noreply.github.com>
2021-02-17 22:46:05 +0100

    Update index.js
    Login allow CORS in Store

--- src/store/index.js
+++ src/store/index.js
@@ -79,6 +79,7 @@ export default new Vuex.Store({
 
                         localStorage.setItem("token", token);
                         axios.defaults.headers.common["Authorization"] = "Bearer  " + token;
+                        axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
                         commit("auth_success", {
                             token: token,
                             refreshToken: refreshToken,
